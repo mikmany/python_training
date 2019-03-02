7768c51e8545c5be0f52386560243dab3842f9a8
Mikhail Menyakin <mikhail.menyakin@gmail.com>
2019-03-02 14:09:17 +0300

    редактирование

--- test_add_group.py
+++ test_add_group.py
@@ -1,58 +1,71 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from group import Group
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="C:/Program Files/Mozilla Firefox ESR/firefox.exe")
+        self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="C:/Program Files/Mozilla Firefox ESR/firefox.exe")
         self.wd.implicitly_wait(60)
-    
+
+
     def test_test_add_group(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group(name="anak", header="vbcjd", footer="djvnm"))
+        self.retern_to_groups_page(wd)
+        self.logout(wd)
+
+    def retern_to_groups_page(self, wd):
+        # возвращение на страницу со списком групп
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        # выход из аккаунта
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_group(self, wd, group):
+        # создание группы
         wd.find_element_by_name("new").click()
+        # открытие формы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("anak")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("vbcjd")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("djvnm")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # клик по созданию
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
+
+    def open_group_page(self, wd):
+        # открытие страницы со списком групп
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd, username, password):
+        # логин
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
-        self.assertTrue(success)
-    
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        # открытие главной станицы
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
